ecb61c190bd47ff00724241c3703ab4502513df5
karkay <kentkfeng@gmail.com>
2019-01-07 15:20:06 -0500

    Updates

---
+++ precache-manifest.fe9233aefeb4dcbaa46fdd400e1a7bbe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f3cac062bb0d23fbc71",
+    "url": "/cuny2x/static/js/runtime~main.8f3cac06.js"
+  },
+  {
+    "revision": "91633c5d80ea10039199",
+    "url": "/cuny2x/static/js/main.91633c5d.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/cuny2x/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "91633c5d80ea10039199",
+    "url": "/cuny2x/static/css/main.c2eca780.chunk.css"
+  },
+  {
+    "revision": "8c1198f383103792819dead6f3dd8130",
+    "url": "/cuny2x/index.html"
+  }
+];
